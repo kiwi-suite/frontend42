29295d346e164588ebc48c18ffd24cc89dc0a87e
Thomas Payer <payer@raum42.at>
2015-08-20 22:14:23 +0200

    added page keywords

--- src/Frontend42/PageType/AbstractPageType.php
+++ src/Frontend42/PageType/AbstractPageType.php
@@ -2,13 +2,28 @@
 namespace Frontend42\PageType;
 
 use Cocur\Slugify\Slugify;
+use Frontend42\Command\Keyword\RefreshPageKeywordsCommand;
 use Frontend42\Model\Page as PageModel;
 use Frontend42\Model\Sitemap;
 use Frontend42\PageType\PageTypeContent;
 use Frontend42\PageType\PageTypeInterface;
+use Frontend42\TableGateway\PageKeywordTableGateway;
 
 abstract class AbstractPageType implements PageTypeInterface
 {
+    /**
+     * @var RefreshPageKeywordsCommand
+     */
+    protected $refreshPageKeywordsCommand;
+
+    /**
+     * @param RefreshPageKeywordsCommand $refreshPageKeywordsCommand
+     */
+    public function setKeywordCommand(RefreshPageKeywordsCommand $refreshPageKeywordsCommand)
+    {
+        $this->refreshPageKeywordsCommand = $refreshPageKeywordsCommand;
+    }
+
     /**
      * @param Sitemap $sitemap
      * @return null
@@ -32,8 +47,11 @@ abstract class AbstractPageType implements PageTypeInterface
      * @param $approved
      * @return mixed
      */
-    public function savePage(PageTypeContent $content, PageModel $page, $approved)
-    {
+    public function savePage(
+        PageTypeContent $content,
+        PageModel $page,
+        $approved
+    ) {
         if ($approved === false) {
             return;
         }
@@ -61,6 +79,10 @@ abstract class AbstractPageType implements PageTypeInterface
             ->setPublishedFrom($publishedFrom)
             ->setPublishedUntil($publishedUntil)
             ->setRoute(json_encode($routing));
+
+        $keywords = trim($content->getParam('keywords'));
+        $keywords = (empty($keywords)) ? [] : explode(',', $keywords);
+        $this->refreshPageKeywordsCommand->setPageId($page->getId())->setKeywords($keywords)->run();
     }
 
     abstract protected function getRouting(PageTypeContent $content, PageModel $page);
