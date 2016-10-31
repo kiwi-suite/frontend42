01e50f07b9664525349b4ce62e9d27a7606011fd
Thomas Payer <payer@raum42.at>
2016-10-31 11:03:55 +0100

    added pageType view script support

--- src/Controller/Frontend/DefaultController.php
+++ src/Controller/Frontend/DefaultController.php
@@ -2,16 +2,32 @@
 namespace Frontend42\Controller\Frontend;
 
 use Core42\Mvc\Controller\AbstractActionController;
+use Frontend42\PageType\Service\PageTypePluginManager;
+use Zend\View\Model\ViewModel;
 
 class DefaultController extends AbstractActionController
 {
+    protected function getViewModel()
+    {
+        $sitemap = $this->getEvent()->getRouteMatch()->getParam("__sitemap__");
+
+        $pageType = $this->getServiceManager()->get(PageTypePluginManager::class)->get($sitemap->getPageType());
+
+        $viewModel = new ViewModel();
+        if (method_exists($pageType, 'getView')) {
+            $viewModel->setTemplate($pageType->getView());
+        }
+
+        return $viewModel;
+    }
+
     public function localizationAction()
     {
-        die("localization");
+        return $this->getViewModel();
     }
 
     public function contentAction()
     {
-        die("content");
+        return $this->getViewModel();
     }
 }
